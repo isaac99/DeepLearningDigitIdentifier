8a777e3216410efaea7f02a2899e7d011481c6fb
Isaac Shiffman <ishiffman120@MacBook-Pro-5.local>
2019-11-21 14:02:23 +0100

    data reader workinggit add -A

--- DataReader.py
+++ DataReader.py
@@ -1,22 +1,77 @@
+from __future__ import division
 import numpy as np
 import h5py
 import tqdm
 import os
+from cv2 import cv2
+import pickle
 
 class DataReader():
-    def __init__(self, folder="./input/test"):
-        mat_data = h5py.File(os.path.join(folder, 'digitStruct.mat'))
+    def __init__(self, folder="./input/train", file='digitStruct', pickle_name=None, pickle_exists=False, development_mode=False):
+        self.folder = folder
+        self.pickle_name = pickle_name
+        self.file = file
+        self.development_mode = development_mode
+
+        data_filename = os.path.join('pickles', pickle_name + "_data.p")
+        image_names_filename = os.path.join('pickles', pickle_name + "_image_names.p")        
+
+        
+
+        if not os.path.isfile(data_filename) or not os.path.isfile(image_names_filename):
+            print('Existing pickle not found!')
+            self.do_data_setup(data_filename, image_names_filename)
+        else:
+            print('Running from existing pickle!')
+            loaded_pickle_data = pickle.load( open( data_filename, "rb" ))
+            loaded_pickle_image_names = pickle.load(open(image_names_filename, "rb"))
+            self.data = loaded_pickle_data
+            self.original_image_names = loaded_pickle_image_names
+        
+    
+        self.labels, self.number_images = self.get_individual_number_images()
+    
+
+    def do_data_setup(self, data_filename, image_names_filename):
+
+        mat_data = h5py.File(os.path.join(self.folder, self.file + '.mat'))
         print(mat_data)
-        size = mat_data['/digitStruct/name'].size
+        size = mat_data['/' + self.file + '/name'].size
+        data = []
+        images = []
 
         for _i in tqdm.tqdm(range(size)):
             pic = self.get_name(_i, mat_data)
             box = self.get_box_data(_i, mat_data)
 
-            print(pic)
-            print(box)
+            # print(pic)
+            # print(box)
 
-    
+            for index, number in enumerate(box['label']):
+                height = int(box['height'][index])
+                width = int(box['width'][index])
+                left = int(box['left'][index])
+                top = int(box['top'][index])
+
+                # print(index)
+                # print(number)
+                # print(height)
+                # print(width)
+                # print(left)
+                # print(top)
+
+                # data = np.append(data, [number, height, width, left, top], axis=0)
+                data.append((number, height, width, top, left, pic))
+                images.append(pic)
+            
+        self.data = np.array(data)
+        self.original_image_names = np.array(images)
+
+        pickle.dump(self.data, open( data_filename, "wb" ))
+        pickle.dump(self.original_image_names, open( image_names_filename, "wb" ))
+
+
+
     def get_box_data(self, index, hdf5_data):
         """
         get `left, top, width, height` of each picture
@@ -47,3 +102,102 @@ class DataReader():
     def get_name(self, index, hdf5_data):
         name = hdf5_data['/digitStruct/name']
         return ''.join([chr(v[0]) for v in hdf5_data[name[index][0]].value])
+
+    def get_individual_number_images(self):
+        image_labels = []
+        number_image_cutouts = []
+
+        for index, image_name in enumerate(self.original_image_names):
+            #if in development mode only return the first 5 images so testing model is faster
+            if self.development_mode is True and index > 300:
+                break
+
+            print('finished image: ' + image_name)
+            path = os.path.join(self.folder, image_name)
+
+            #read image in grayscale
+            image = cv2.imread(path, 0)
+            digits_in_image = self.data[self.data[:, 5] == image_name]
+
+            for i, digit in enumerate(digits_in_image):
+                label = int(float(digit[0]))
+                height = int(float(digit[1]))
+                width = int(float(digit[2]))
+                top = int(float(digit[3]))
+                left = int(float(digit[4]))
+
+                digit_image = image[top: top+height, left:left+width]
+
+                digit_image = resize_if_necessary(digit_image)
+
+                black_canvas = np.zeros((224, 224), dtype=np.uint8)
+                black_canvas[0:digit_image.shape[0], 0:digit_image.shape[1]] = digit_image
+
+                final_digit_image = black_canvas
+
+                number_image_cutouts.append(final_digit_image)
+                image_labels.append(label)
+
+        return np.array(image_labels), np.array(number_image_cutouts)
+
+
+
+# def resize_height(digit_image):
+#     aspectRatio = digit_image.shape[1] / digit_image.shape[0]
+
+
+#     return digit_image
+
+
+# def resize_width(digit_image):
+
+#     return digit_image
+
+# def resize_image()
+
+def resize_if_necessary(image):
+    height = image.shape[0]
+    width = image.shape[1]
+
+    # aspect_ratio = height / width
+
+
+    # print(image.shape)
+
+    # print(height)
+    # print(width)
+
+    if width > 224 or height > 224:
+        if width >= height:
+            aspect_ratio = height / width
+            # print(aspect_ratio)
+            new_width = 224
+            new_height = aspect_ratio * new_width
+
+            new_size = (int(new_width), int(new_height))
+            # print(new_size)
+
+
+            resized_image = cv2.resize(image, new_size, interpolation = cv2.INTER_AREA)
+            return resized_image
+            # width_ratio = 224/width
+
+
+        elif height > width:
+            aspect_ratio = width / height
+            new_height= 224
+            new_width = aspect_ratio * new_height
+
+
+
+            new_size = (int(new_width), int(new_height))
+
+
+            # print(new_size)
+
+
+            resized_image = cv2.resize(image, new_size, interpolation=cv2.INTER_AREA)
+            return resized_image
+
+    else:
+        return image
--- experiment.py
+++ experiment.py
@@ -1,10 +1,12 @@
 import numpy as np
-import cv2
+from cv2 import cv2
 import tensorflow as tf
 from tensorflow import keras
 from keras_preprocessing.image import ImageDataGenerator
+import argparse
 from VGG16 import VGG16
 from DataReader import DataReader as reader
+import pickle
 
 def main():
     # print('running')
@@ -12,7 +14,28 @@ def main():
     # training_data = training_data_generator.flow_from_directory(directory="./input/train", target_size=(224, 224))
     # testing_data_generator = ImageDataGenerator()
     # testing_data = testing_data_generator.flow_from_directory(directory="./input/test", target_size=(224, 224))
-    training_data = reader('input/test')
+
+    # training_data32x32 = reader(folder='input/train', file='train_32x32', pickle_name='training32x32')
+    # exit(0)
+
+    training_data_reader = reader(folder='input/train', file='digitStruct.mat', pickle_name='training', development_mode=True)
+    images = training_data_reader.number_images
+    labels = training_data_reader.labels
+    # print(labels[0])
+    # cv2.imshow('test', images[0])
+    # cv2.waitKey()
+    # exit(0)
+
+
+
+    # print(training_data.number_images[0][0])
+    # cv2.imshow('test', training_data.number_images[0][1])
+    # cv2.waitKey()
+
+
+    # testing_data = reader('input/test')
+
+    # print(training_data.data[:20, :])
 
     pass
 
